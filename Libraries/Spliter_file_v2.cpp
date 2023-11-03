6bd08d0b2d818299e827add374c5ef93edc72ba9
Kadda Aoues <houes_k@yahoo.fr>
2023-11-03 11:12:31 +0300

    Version 2 of spliter

---
+++ Libraries/Spliter_file_v2.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <MyLib/opendialogfile.h>                // using opendialog windows
+#include <MyLib/Console_Library/escape_code.h>   // for text coloring
+#include <MyLib/CFile.h>						 // using class CFile
+#include <MyLib/chrono/to_day.h>                 // using to day class to generate date.
+
+/*
+                Author : KADDA Aoues 
+				Date   : 3 / 11 / 2023
+				Title  : version n 2 of spliter.assembler file.
+
+*/
+
+void splite_file(const fs::path& file_name, size_t Size) {
+
+}
+
+void assemble_files(const fs::path& directory) {
+
+}
+
+
+int main() {
+	Print_(color::Yellow, "******  KADDA Aoues sofware ware presente ******") << end_;
+	Print_(color::Yellow, "******  spliter version v 2.0 *****") << end_;
+
+	// here we load file, check file existence and sizing
+
+
+
+	// here check directory existing and splites files existing and appropriate names for original
+	// file.
+
+	wait_;
+	return 0;
+}
