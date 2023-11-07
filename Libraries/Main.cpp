fb820e0bd2145119ecb74db8a4a9b482e57f0ffc
Kadda Aoues <houes_k@yahoo.fr>
2023-11-07 06:49:15 +0300

    start with console utility

--- Libraries/Main.cpp
+++ Libraries/Main.cpp
@@ -6,19 +6,22 @@
                          1. container utility : namespace container_utility = cu::
                          2. associative container utility : namespace        acu::
                          3. file utility :                                  File::
-                                   3.1 make splite filer 31 / 10 / 2023
+                             3.1 make splite filer 31 / 10 / 2023
                          4. bitwise utility :                                Bit::
                          5. bitmap image utility :                          bmpf::
                          6. escape character utility :                       Esc::
                          7. string utility                                   Str::
-                                   7.1 we need to change get_text_word from using 
-                                   vector to using map to avoid repetition.
+                             7.1 we need to change get_text_word from using 
+                                 vector to using map to avoid repetition.
                          8. Time utility                                    Time::
                              8.1 to_day namespace and class
                              8.2 functionality of time
                          9. Calender utility                            Calender::
                          10. meta programming lib                            mpl::
                          11. function utility mathematic and logic      Function::
+                             11.1 function of clampings
+                             11.2 function factorial and is based on Gamma 
+                                  funcions familly.
                          12. random variable utility                         RND::
                          13. variable transition                              vt::
                          14. geometry function and relation                geo2d:: 
