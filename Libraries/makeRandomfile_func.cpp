dc204a28d2cfdaa7f05fdd3af4b5ae4e3bb794f7
Kadda Aoues <houes_k@yahoo.fr>
2023-11-15 20:10:31 +0300

    modifie makeRandomTextFile to be handle any function generator of string(word) .

--- Libraries/makeRandomfile_func.cpp
+++ Libraries/makeRandomfile_func.cpp
@@ -3,26 +3,50 @@
 #include "MyLib/Console_Library/escape_code.h"
 #include <MyLib/CFile.h>
 
-
+/*
+           Auture : KADDA Aoues 
+		      Exemple for filesystem, and using opendialgue folder box, 
+			  and test making random file text and binary.
+*/
 
 
 int main()
 {
-	auto file_name = opendialog::OpenFile(L"get any valide file");
 
-	fs::path file_path{ file_name };
+	fs::path folder = fs::path(opendialog::OpenFolder(L""));
 
-	print_ << "file : " << COLOR(color::Green, file_path.string()) << end_;
-	print_ << "file name : " << COLOR(color::DarkGreen, file_path.stem().string()) << end_;
+	if (!fs::exists(folder)) {
+		Print_(color::Red, "this folder : ") << folder.string() << COLOR(color::Green, "not exist") << end_;
+		return -1;
+	}
+	else {
+		Print_(color::Green, "folder : ") << folder.string() << end_;
+	}
+
+	fs::path file_random_text = folder.string() + "\\Random_text.txt";
+	fs::path file_random_binary = folder.string() + "\\Random_binary.bin";
+
+	print_ << "text file   : " << COLOR(color::Red, file_random_text.string()) << end_;
+	print_ << "binary file : " << COLOR(color::Red, file_random_binary.string()) << end_;
 	wait_;
 
+	// checking if file exist otherwise will not forward operation
 
-	fs::path new_file = fs::path(opendialog::OpenFolder(L"")).string() + "\\Random_file.txt";
+	if (!fs::exists(file_random_text)) {
+		Print_(color::Red, "file not exit ok!") << end_;
+	    File::makeRandomTextFile(file_random_text,100, 100 ,114, Str::getRandomString, "A  ", 99ull);
+	}
+	else {
+		Print_(color::Red, "file exist do nothing") << end_;
+	}
 
-	print_ << "new file : " << COLOR(color::Red, new_file.string()) << end_;
-	wait_;
-
-	File::makeRandomFile(new_file, 1000);
+	if (!fs::exists(file_random_binary)) {
+		Print_(color::Red, "file not exit ok!") << end_;
+	    File::makeRandomFile(file_random_binary, 1ull << 7ull);
+	}
+	else {
+		Print_(color::Red, "file exist do nothing") << end_;
+	}
 
 	std::cin.get();
 	return 0;
