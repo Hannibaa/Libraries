2144d12fbfef53281175662376d7691400273b08
Kadda Aoues <houes_k@yahoo.fr>
2023-11-19 01:29:08 +0300

    get chart for wchar_t types

---
+++ Libraries/wchar_t_chart.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include "MyLib/Console_Library/Event_Windows.h"
+#include "MyLib/Console_Library/escape_code.h"
+
+
+int main()
+{
+	esc::init_wchar;
+
+	print_wchar_chart(0x1fff, 0x2fff);
+
+
+	std::cin.get();
+	return 0;
+}
