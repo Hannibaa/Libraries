81a1e0a1c86b085e8e273394e63db49bc9eee867
Kadda Aoues <houes_k@yahoo.fr>
2023-11-19 23:39:48 +0300

    add new functionality of transformation, new using fire

--- Libraries/console_game_1.cpp
+++ Libraries/console_game_1.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <sstream>
+#include <array>
 #include <thread>
 #include "MyLib/Console_Library/Event_Windows.h"
 #include "MyLib/random_variable.h"
@@ -60,11 +61,12 @@ public:
 class Text_ {
 
 	std::wstring      text;
+	std::array<std::wstring, 4>  _shapes;
 	float             x, y;
 	float         _vx, _vy;
 	color           _color;
-
-	int up, down, right, left;
+	int i{};
+	int up, down, right, left , select_key;
 
 	void moving(float& x, float& y, float speedX, float speedY) {
 		if (KeyPressed(up))    y -= speedY;
@@ -74,6 +76,13 @@ class Text_ {
 
 		x = std::clamp<float>(x, 0.f, _width);
 		y = std::clamp<float>(y, 0.f, _height);
+	}
+
+	void select_shape() {
+		++i;
+		i = i % 4;
+		if (KeyPressed(select_key))
+			if ( i == 0 || i == 1 || i == 2 || i == 3 ) text = _shapes[i];
 	}
 
 public:
@@ -88,14 +97,22 @@ public:
 		down{VK_DOWN},
 		right{VK_RIGHT},
 		left{VK_LEFT},
+		select_key{VK_SPACE},
 		_color{ static_cast<color>(clr) }
 	{}
 
-	void define_control(int _up, int _down, int _right, int _left) {
+	void define_control(int _up, int _down, int _right, int _left,int _select_key) {
 		up = _up;
 		down = _down;
 		right = _right;
 		left = _left;
+		select_key = _select_key;
+	}
+
+	void insert_shape(const std::wstring (& shape)[3]) {
+		_shapes[1] = shape[1];
+		_shapes[2] = shape[2];
+		_shapes[3] = shape[0];
 	}
 
 	void setX(float a ) {
@@ -113,14 +130,17 @@ public:
 
 	void setText(std::wstring_view _text) {
 		text = _text;
+		_shapes[0] = text;
 	}
 
 	void setText(const wchar_t w) {
 		text.push_back(w);
+		_shapes[0] = text;
 	}
 
 	std::wstring update() {
 		moving(x, y, _vx, _vy);
+		select_shape();
 		std::wstringstream str;
 		int _x = x; int _y = y;
 		str << MOVETO(_x, _y) << _wCOLOR_FG256(_color) << text << RESETMODE;
@@ -139,20 +159,30 @@ int main()
 {
 	INIT_WCHAR_TEXT;
 	WINIT_CURSOR;
-    
+
 	std::chrono::milliseconds _ms = 70ms;   // timing and speed
 
 
-	Text_ text(20.f,10.f, 10 ,{3.3f,1.3f});
+	Text_ text(20.f, 10.f, 6, { 3.3f,1.3f });
 	text.setText(Unicode::solid[9]);
 	text.setText(Unicode::solid[9]);
 	text.setText(Unicode::solid[9]);
+
+	{
+		std::wstring shapes3[3]{ {Unicode::solid[0],Unicode::solid[3],Unicode::solid[0],L'0'},
+		L"\x2584\x2588\x2584" , L"\x25ac\x2590\x25ac" };
+
+	    text.insert_shape(shapes3);
+	}
 
 	Text_ text2(60.f,10.f, 10 ,{3.3f,1.3f});
 	text2.setText(Unicode::solid[9]);
 	text2.setText(Unicode::solid[9]);
 	text2.setText(Unicode::solid[9]);
-	text2.define_control(_u('W'), _u('S'), _u('A'), _u('D'));
+	text2.define_control(_u('W') , _u('S'), _u('D'),_u('A'), VK_BACK);
+
+	std::wstring shapes3[3] {L"<.\x2588\x2588.>", L"[:.\x2588.:]" , L""};
+	text2.insert_shape(shapes3);
 
 	wchar_t title[100]{};
 
@@ -169,14 +199,12 @@ int main()
 
 
 		wprint_ << text.update();
-		wprint_ << text.update();
-		wprint_ << text.update();
 		wprint_ << text2.update();
 		
 
 		if (KeyPressed(VK_ESCAPE)) break;
 
-		if (KeyPressed(VK_SPACE)) {
+		if (KeyPressed(VK_CONTROL)) {
 			put_string_at(0, 2, L"Press up to move up");
 			put_string_at(0, 3, L"Press down to move down");
 			put_string_at(0, 4, L"Press right to move right");
