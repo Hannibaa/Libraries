97189820a64ee6b678dec1ee0cff6493d6aaa052
Kadda Aoues <houes_k@yahoo.fr>
2023-11-28 08:37:55 +0300

    updating histogram

--- Libraries/histogram.cpp
+++ Libraries/histogram.cpp
@@ -16,21 +16,61 @@ void put_string_horizontal_at(int x, int y, const std::wstring& str, int color) 
 	wprint_ << RESTORCUR_SAVED_POS;
 }
 
+float v{};
+int   index{}, index_r{};
+std::vector<float> values = vu::makeRandomVector<float>(10.f, 200.f, 50);
+wchar_t title[100]{};                                  // title console.
+
+void menu() {
+
+	
+	while (1) {
+
+		// Menu of console
+		if (KeyPressed(VK_SPACE)) {
+			put_string_at(0, 2, L"Press up to add element falling");
+			put_string_at(0, 3, L"Press down to remove element");
+			put_string_at(0, 4, L"Press A to change game parameter");
+			put_string_at(0, 5, L"Press left  N\\A");
+			wwait_;
+
+		}
+
+		// break
+		if (KeyPressed(VK_ESCAPE)) {
+			break;
+		}
+
+		// reading the value of vector 
+		if (KeyReleased(VK_UP)) {
+			if (!values.empty()) {
+				index_r = index;
+				index = std::clamp(++index,0,(int)values.size());
+				if (index < values.size() && index >=0 )
+				v = values[index];
+			}
+		}
+		if (KeyReleased(VK_DOWN)) {
+			if (!values.empty()) {
+				index_r = index;
+				index = std::clamp(--index,0,(int)values.size());
+				if (index < values.size() && index >= 0)
+				v = values[index];
+			}
+		}
+
+	}
+	
+
+}
+
 int main()
 {
-
+	std::thread   thread_(menu);
 	INIT_WCHAR_TEXT;                                       // initialization of wchar_t text.
 	WINIT_CURSOR;                                          // hide the cursor.
 	std::chrono::milliseconds game_elapsetime_ms = 70ms;   // timing and speed.
-	wchar_t title[100]{};                                  // title console.
 	
-
-	
-	// Title of Console 
-	swprintf_s(title, L"  Game Moving Frame per sec [%5.3f] |Time[%d]|PosX[%3d]|PosY[%3d] | Collision Times[%5d]",
-		  1.f, 1, 1, 1, 1);
-	put_string_at(30, 0, title, color::Red);
-	TITLE(title);
 
 	// set the constante of histogram:
 	const int histoX{13};                               //
@@ -50,7 +90,7 @@ int main()
 	const int histo_title_Yx{ histoX - 4 };
 
 	std::vector<ProgressBarH> bars;
-	std::vector<float> values = vu::makeRandomVector<float>(10.f, 200.f, histo_max_bars);
+	//std::vector<float> values = vu::makeRandomVector<float>(10.f, 200.f, histo_max_bars);
 	float value_max = *std::max_element(values.begin(),values.end());
 
 	for (int i = 0; i != histo_max_bars; ++i) {
@@ -62,6 +102,8 @@ int main()
 		bars[bars.size() - 1].set_width(histo_bar_width);
 		float v_percent = 100.f * values[i] / value_max;
 		bars[bars.size() - 1].set_value(v_percent);
+		int col = i % 2 ? 237 : color::Yellow;
+		bars[bars.size() - 1].setBackground_color(col);
 	}
 
 
@@ -77,23 +119,42 @@ draw_loop:
 	for (auto& b : bars) b.draw();
 	barr.draw();
 
+	while (1) {
 
-	// Menu of console
-	if (KeyPressed(VK_SPACE)) {
-		put_string_at(0, 2, L"Press up to add element falling");
-		put_string_at(0, 3, L"Press down to remove element");
-		put_string_at(0, 4, L"Press A to change game parameter");
-		put_string_at(0, 5, L"Press left  N\\A");
-		wwait_;
-		goto draw_loop;
+		std::this_thread::sleep_for(100ms);
+		esc::wclear_line(0, 0);
+		// Title of Console 
+		swprintf_s(title, L"Histogram v.1 vector[%d][%f] |",
+			index, v);
+		put_string_at(30, 0, title, color::Red);
+		TITLE(title);		
+
+		if (index > -1 && index < bars.size())
+		put_char_at(bars[index].getPosition().x, bars[index].getPosition().y, '\x1f', color::Red);
+		if (index_r > -1 && index_r < bars.size())
+		put_char_at(bars[index_r].getPosition().x, bars[index_r].getPosition().y, '\x1f', color::Black);
+
+		// Menu of console
+		/*if (KeyPressed(VK_SPACE)) {
+			put_string_at(0, 2, L"Press up to add element falling");
+			put_string_at(0, 3, L"Press down to remove element");
+			put_string_at(0, 4, L"Press A to change game parameter");
+			put_string_at(0, 5, L"Press left  N\\A");
+			wwait_;
+			goto draw_loop;
+		}*/
+
+		// break
+		if (KeyPressed(VK_ESCAPE)) {
+			break;
+		}
 	}
-
 
 
 
 	//wprint_ << _wCOLOR_BG256(12) << "Exit Game..." << wend_;
 	//std::wcout << RESETMODE;
-
+	thread_.join();
 	std::cin.get();
 	return 0;
 }
