e106be34123ccce964e6f6ec02be920de6d8ca7e
Kadda Aoues <houes_k@yahoo.fr>
2023-12-29 09:44:32 +0300

    exemple for mesure_bar

---
+++ Libraries/Histogram/mesure_bar.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <thread>
+#include "mesured_value.h"
+
+
+
+
+
+int main()
+{
+	INIT_WCHAR_TEXT;
+	WHIDE_CURSOR;
+
+	cui::Mesured_value  flow_bar(10 ,25, -10.5f,+11.6f, L"FlowIn",L"Gal/min" ,
+		                          10,3,color::Fuchsia,color::Grey11);
+
+
+	int count{ 3 };
+
+	while (--count) {
+		 float value = float(rand() % 10);
+		 flow_bar.set_value(value);
+	     flow_bar.draw();
+		 wwait_;
+		 std::this_thread::sleep_for(std::chrono::milliseconds(100));
+		 esc::wcls();
+	}
+
+
+
+	std::wcin.get();
+	return 0;
+}
