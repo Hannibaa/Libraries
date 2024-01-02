fa8fce327c8781226665ff893840beeb923b44ed
Kadda Aoues <houes_k@yahoo.fr>
2024-01-03 00:49:18 +0300

    image ascii exemple

--- Libraries/Image/ascii_image_2.cpp
+++ Libraries/Image/ascii_image_2.cpp
@@ -4,31 +4,19 @@
 #include "MyLib/Console_Library/escape_code.h"
 #include "MyLib/stringfunctionhelper.h"
 
-// todo make library for convert to image ascii 
-template<int Xmax>
-int Intensity(int x, int L) {
-	const int l    = Xmax / L;
-	
-	for (int j = 0; j != L - 1; ++j) {
-		if (x >= l * j && x < l * (j + 1)) return j;
-	}
-
-	if (x >= l * (L - 1) && x < Xmax + 1) return L - 1;
-
-	return 0;
-}
 
 int main()
 {
+	DEFINE_rgb2ansi
+
 	COMDLG_FILTERSPEC bmp_ext{ L"bitmap .bmp", L"*.bmp" };
-	auto image_file =Str::to_char<char>(opendialog::OpenFile(L"GET FILE BITMAP .BMP", bmp_ext));
+	auto image_file = Str::to_char<char>(opendialog::OpenFile(L"GET FILE BITMAP .BMP", bmp_ext));
 
 	if (image_file.empty()) {
 		Print_(color::Red, "Error...") << end_;
 		return -1;
 	}
 
-	std::string pattern{ "#@$%&*_." };
 
 	bitmap_image image(image_file);
 
@@ -38,12 +26,7 @@ int main()
 	Print_(color::Green, "dimension of image :") << image.width() << " "
 		<< image.height() << end_;
 
-	std::ofstream ofs{ "image.txt" };
-
-	if (!ofs.is_open()) {
-		Print_(color::Red, "Error...") << end_;
-		return -1;
-	}
+	
 
 	for (int i = 0; i != w; ++i)
 	{
@@ -51,13 +34,10 @@ int main()
 		{
 			auto rgb = image.get_pixel(i, j);
 			int _color = RGB2INT(rgb.red, rgb.blue, rgb.blue);
-			int _pos = Intensity<0xff>(int(rgb.red), 8);
-			ofs << pattern[_pos];
+			printm_(i,j) <<_COLOR_BG256(rgb2ansi[_color]) << " " << RESETMODE;
 		}
-		ofs << end_;
 	}
 
-	ofs.close();
 	print_ << "End....." << end_;
 	std::cin.get();
 	return 0;
