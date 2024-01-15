134c13d832ec9e7b570dce0d5686d581c2b2eafe
Kadda Aoues <houes_k@yahoo.fr>
2024-01-16 02:27:23 +0300

    add new comment

--- Libraries/Main.cpp
+++ Libraries/Main.cpp
@@ -16,27 +16,38 @@
                              1.1 printing function
                              1.2 first Adaptor implemented
                              1.3 function get_Interval(container,value).
+
                          2. associative container utility : namespace        acu::
+
                          3. file utility :                                  File::
                              3.1 make splite filer 31 / 10 / 2023
                              3.2 add multiple functionality on it 11 / 11 / 2023
                              3.3 finalize splite files as functions.
+
                          4. bitwise utility :                                Bit::
-                         5. bitmap image utility :                          bmpf::
-                         6. escape character utility :                       Esc::
+
+                         5. bitmap image utility :                           Bmp::
+
+                         6. escape character utility :                       esc::
+
                          7. string utility                                   Str::
                              7.1 we need to change get_text_word from using 
                                  vector to using map to avoid repetition.
+
                          8. Time utility                                    Time::
                              8.1 to_day namespace and class
                              8.2 functionality of time
+
                          9. Calender utility                            Calender::
+
                          10. meta programming lib                            mpl::
+
                          11. function utility mathematic and logic      Function::
                              11.1 function of clampings
                              11.2 function factorial and is based on Gamma 
                                   funcions familly.
                              11.3 add clamp function
+
                          12. random variable utility                         RND::
                          13. variable transition                              vt::
                          14. geometry function and relation                geo2d:: 
