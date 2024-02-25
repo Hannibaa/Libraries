0b759f0f54beeed06be7fe0f69f0b18c723d9ff7
Kadda Aoues <houes_k@yahoo.fr>
2024-02-25 04:25:36 +0300

    using thread with osyncstream

--- Libraries/console_1.cpp
+++ Libraries/console_1.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <syncstream>
 #include <thread>
 #include <vector>
 #include <MyLib/Console_Library/escape_code.h>
@@ -10,23 +11,24 @@
 
 using namespace std::literals;
 
-void effect(int x, int y, char c, std::chrono::milliseconds seconds) {
+void effect(int x, int y, std::string_view _string, std::chrono::milliseconds seconds) {
 	for (int i = 232; i != 266; ++i) {
-		print_ << MOVETO( x, y);
-		print_ << _COLOR_FG256(i) << c << "hello_" << i;
+		
+		std::osyncstream(print_)  << MOVETO( x, y) << _COLOR_FG256(i) << _string 
+			                      << "hello_" << i << RESETMODE;
+
 		std::this_thread::sleep_for(seconds);
 	}
 }
 
 int main() {
 
-	std::thread t1(effect, 10, 10, 'A', 100ms);
-	std::thread t2(effect, 20, 20, 'B', 250ms);
+	std::thread t1(effect, 10, 10, "thread 111", 100ms);
+	std::thread t2(effect, 20, 20, "thread 200", 250ms);
 	//std::thread t3(effect, 0, 0);
 
-	effect(100, 13, 'C', 250ms);
+	effect(100, 13, "Main Thread", 250ms);
 
-	print_ << RESETMODE;
 
 	t1.detach();
 	t2.detach();
